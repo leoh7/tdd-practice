14bb3ab95936f0e0399bcd4e25e6df872e861e83
JUN-HO. H <develop.leoh7@gmail.com>
2020-07-20 15:43:38 +0900

    📝 Add practice plan

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 import { shallow } from 'enzyme';
-
+/**
+ * 
+ * Todo list 계획
+ * 1. 할 일을 적을 수 있는 칸이 있다
+ * 2. 칸에 글씨를 채워넣고 입력을 하면 리스트가 된다
+ * 3. 리스트는 체크해서 완료 처리 할 수 있다
+ * 
+ */
 describe('', () => {
-  it('', () => {
-    const appWrapper = shallow(<App />)
+  it('should render', () => {
+    const appWrapper = shallow(<App />).
+    expect(appWrapper).toHaveLength(1)
   });
 })
 
